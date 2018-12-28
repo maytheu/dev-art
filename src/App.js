d5d0f4465ab97295679046797ddb6b50f83ebf24
Laue <maytheu98@gmail.com>
2018-12-28 11:33:21 +0100

    initial router state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+
 import './App.css';
+const Header = () => <h2>Header</h2>
+const Menu = () => <h2>Menu</h2>
+const Landing = () => <h2>Homepage</h2>
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello Young creator
+        <Header />
+        <Menu />
+        <Route exact path="/" component={Landing}/>
       </div>
     );
   }
